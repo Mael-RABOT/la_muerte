e5cb0a60fa41b93d52724dfd8998ed03ce46f2de
Maël RABOT <mael.rabot@gmail.com>
2022-11-08 22:41:13 +0100

    Create la_muerte.c
    la_muerte is here, run while you still can.

---
+++ la_muerte.c
@@ -0,0 +1,17 @@
+/*
+** la_muerte, 2022
+** la_muerte
+** file description:
+** la_muerte.c
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(void)
+{
+  int *i;
+  for (;;)
+    i = malloc(sizeof(i) * 100);
+  return (EXIT_SUCCESS);
+}
